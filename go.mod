dd167b8d741353a2433135094a99cc4fefdf3aa2
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-11-20 15:50:35 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dmytrochumakov/gator
+
+go 1.22.5
---
+++ internal/config/config.go
@@ -0,0 +1,71 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(configFilePath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	config := Config{}
+	err = decoder.Decode(&config)
+
+	if err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+}
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(homeDir, configFileName), nil
+}
+
+func (c *Config) SetUser(username string) error {
+	c.CurrentUserName = username
+	return write(*c)
+}
+
+func write(cfg Config) error {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(configFilePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
